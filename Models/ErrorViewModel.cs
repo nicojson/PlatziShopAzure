abdf27174b988ba514da6181a1c13d3090ce683a
nico_json <nicolasgodinez.nssg@gmail.com>
2022-05-03 19:01:04 -0500

    Integramos el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatziShopsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
